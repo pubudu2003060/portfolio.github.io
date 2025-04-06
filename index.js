5b215899c95c713d573820240d76bb8f41269911
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-04-06 14:09:01 +0530

    version one is completed

--- index.js
+++ index.js
@@ -22,3 +22,18 @@ function showTab(tabName) {
 document.addEventListener('DOMContentLoaded', () => {
   showTab('languages');
 });
+
+
+
+  document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    emailjs.sendForm("service_2l35k38", "template_z4zmuc7", this)
+      .then(function () {
+        alert("Message sent successfully!");
+        document.getElementById("contact-form").reset();
+      }, function (error) {
+        alert("Failed to send message. Error: " + JSON.stringify(error));
+      });
+  });
+
