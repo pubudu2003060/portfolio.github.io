f1ce5004f2fdbb94ff1190e280c78ca479cee3fc
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-04-06 21:09:52 +0530

    version two is completed

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-// Menu Toggle
 const menuBtn = document.getElementById('menu-btn');
 const mobileMenu = document.getElementById('mobile-menu');
 
 menuBtn.addEventListener('click', () => {
-  mobileMenu.classList.toggle('hidden');
+  mobileMenu.classList.toggle('active');
 });
 
-// Tab functionality
+
 function showTab(tabName) {
   // Hide all tab contents
   let contents = document.querySelectorAll('.tab-content');
   contents.forEach(content => {
-    content.classList.add('hidden');
+    content.style.display = 'none';
   });
 
   // Show the clicked tab's content
-  document.getElementById(tabName).classList.remove('hidden');
+  document.getElementById(tabName).style.display = 'block';
 }
 
-// Set default tab to 'languages'
 document.addEventListener('DOMContentLoaded', () => {
   showTab('languages');
 });
 
 
 
-  document.getElementById("contact-form").addEventListener("submit", function (e) {
-    e.preventDefault();
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    emailjs.sendForm("service_2l35k38", "template_z4zmuc7", this)
-      .then(function () {
-        alert("Message sent successfully!");
-        document.getElementById("contact-form").reset();
-      }, function (error) {
-        alert("Failed to send message. Error: " + JSON.stringify(error));
-      });
-  });
+  emailjs.sendForm("service_2l35k38", "template_z4zmuc7", this)
+    .then(function () {
+      alert("Message sent successfully!");
+      document.getElementById("contact-form").reset();
+    }, function (error) {
+      alert("Failed to send message. Error: " + JSON.stringify(error));
+    });
+});
 
--- tailwind.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    darkMode: 'class', // or 'media' if you want to use the user's system preference
-    // ...
-  }
-  
