fdbc612f50b7f071b941b0aff34f09309c25b92a
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-04-07 21:12:59 +0530

    make changes to header and hero and intro sections

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ menuBtn.addEventListener('click', () => {
   mobileMenu.classList.toggle('active');
 });
 
+// Close the mobile menu when clicking outside of it
+document.addEventListener('click', (e) => {
+  if (!mobileMenu.contains(e.target) && !menuBtn.contains(e.target)) {
+    mobileMenu.classList.remove('active');
+  }
+});
+
 //about section function
 function showTab(tabName) {
   let contents = document.querySelectorAll('.tab-content');
@@ -35,4 +42,17 @@ document.getElementById("contact-form").addEventListener("submit", function (e) 
 (function () {
   emailjs.init("d8eG9GfvXDYoTRI_p"); 
 })();
+
+//header scroll function
+window.addEventListener('scroll',() => {
+  const header = document.querySelector('header');
+  if(window.scrollY>100){
+    header.classList.add('headerScroll')
+  }else{
+    header.classList.remove('headerScroll')
+  }
+})
+
+
+
 
